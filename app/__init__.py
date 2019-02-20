1282a8fc999dfa6ab219927eb2ec07c7ebd46148
roxaline <mariadelarosita078@gmail.com>
2019-02-20 17:03:21 +0200

    edition

--- app/__init__.py
+++ app/__init__.py
@@ -10,23 +10,3 @@ app.config.from_pyfile('config.py')
 
 from app import views
 
-# def create_app(config_name):
-
-#     app = Flask(__name__)
-
-#     # Creating the app configurations
-#     app.config.from_object(config_options[config_name])
-
-#     #Initializing flask extensions
-#     bootstrap.init_app(app)
-
-#     # Registering the blueprint
-#     from .main import main as main_blueprint
-#     app.register_blueprint(main_blueprint)
-
-#     # setting config
-#     from .requests import configure_request
-#     configure_request(app)
-
-#     # Will add the views
-#     return app
